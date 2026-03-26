d5f6c9a35a236c3f359f0a9ac5b2b320d1da01fc
Tester <dragaomatadordeporco@gmail.com>
2026-03-25 23:09:16 -0300

    Add files via upload

---
+++ module_04/ex00/Cat.cpp
@@ -0,0 +1,33 @@
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+	_type = "Cat";
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &src)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	*this = src;
+}
+
+Cat &Cat::operator=(const Cat &src)
+{
+	if (this != &src)
+	{
+		std::cout << "Cat operator constructor called" << std::endl;
+		_type = src._type;
+	}
+	return *this;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat Destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow Meow" << std::endl;
+}
