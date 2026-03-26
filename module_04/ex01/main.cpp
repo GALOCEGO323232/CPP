999210bdd2accebee49fc055c863215b6a87c575
Tester <dragaomatadordeporco@gmail.com>
2026-03-26 14:35:51 -0300

    Add files via upload

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -1,14 +1,31 @@
-#include "FragTrap.hpp"
+#include "WrongAnimal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
+#include "Animal.hpp"
 
 int main()
 {
-    FragTrap a("Max");
+    std::cout << "\n=== Normal test ===" << std::endl;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound();
+    j->makeSound();
+    meta->makeSound();
 
-    a.attack("enemy");
-    a.highFivesGuys();
-
-    FragTrap b(a);
-    b.attack("target");
-
-    return 0;
+    std::cout << "\n=== Array test ===" << std::endl;
+    Animal* animals[10];
+    for (int i = 0; i < 5; i++)
+        animals[i] = new Dog();
+    for (int i = 5; i < 10; i++)
+        animals[i] = new Cat();
+    for (int i = 0; i < 10; i++)
+        delete animals[i];
+    
+    std::cout << "\n=== deep copy test ===" << std::endl;
+    Dog a;
+    Dog b(a);
 }
