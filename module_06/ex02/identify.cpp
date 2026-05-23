32c68a04f07515f5c358a0e7a6bf0324267d7e2c
Tester <dragaomatadordeporco@gmail.com>
2026-05-23 15:27:06 -0300

    Refactor identify functions for clarity

--- module_06/ex02/identify.cpp
+++ module_06/ex02/identify.cpp
@@ -18,21 +18,21 @@ void identify(Base& p)
 {
 	try
 	{
-		A& a = dynamic_cast<A&>(p);
+		dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		try
 		{
-			B& b = dynamic_cast<B&>(p);
+			dynamic_cast<B&>(p);
 			std::cout << "B" << std::endl;
 		}
 		catch(const std::exception& e)
 		{
 			try
 			{
-				C& c = dynamic_cast<C&>(p);
+				dynamic_cast<C&>(p);
 				std::cout << "C" << std::endl;
 			}
 			catch(const std::exception& e)
